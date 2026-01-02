2b03c989677d6dd94c71559525c87a0cbe304f8d
Ste0912 <cgpa331@gmail.com>
2026-01-02 12:18:07 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,18 @@
 from fastapi import FastAPI
+from fastapi.staticfiles import StaticFiles
+from fastapi.responses import RedirectResponse
 from celery.result import AsyncResult
 from worker import create_dummy_task
 
 
 app = FastAPI()
+
+app.mount("/static", StaticFiles(directory="static", html=True), name="static")
+
+#Redirect root URL (/) to the dashboard
+@app.get("/")
+async def root():
+    return RedirectResponse(url="/static/index.html")
 
 # POST endpoint to submit a Model Name
 @app.post("/submit_eval")
